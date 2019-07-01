cd960789f6ce7b9db854360de7579216503d9ac0
Naeramarth <Blood.Work@gmx.at>
2019-07-01 17:36:16 +0200

    added build directory to git

---
+++ build/precache-manifest.4d74490e5192a741714692ac24401059.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48945e4f2c999dd51dfc5b1fee342d5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b4710e4636cf174609",
+    "url": "/static/css/main.4b9ab9a6.chunk.css"
+  },
+  {
+    "revision": "da90cf7e9d0d3b80857f",
+    "url": "/static/js/2.f175fc13.chunk.js"
+  },
+  {
+    "revision": "b3b4710e4636cf174609",
+    "url": "/static/js/main.5badd227.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
